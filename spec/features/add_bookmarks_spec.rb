b57f2a20f032f77393a72c5743611bdba5680492
Anh Nguyen <anhnguyenis@gmail.com>
2019-11-28 14:50:53 +0000

    refactor dummy data in add_bookmarks_spec.rb

--- spec/features/add_bookmarks_spec.rb
+++ spec/features/add_bookmarks_spec.rb
@@ -1,7 +1,10 @@
 
 feature 'Add bookmarks' do
   scenario 'saves a bookmark' do
-    setup_table
+    #setup_table
+    Bookmark.add(url: "http://www.makersacademy.com") #replaces web_helper named setup_table
+    Bookmark.add(url: "http://www.bbc.co.uk")
+    Bookmark.add(url: "http://www.google.com")
     visit ('/add-bookmark')
     fill_in "url", with: "www.myspace.com"
     click_button("Submit")
