8d681f41bfd46fe1adc1e535ba1de30ec6e5a2f2
Anh Nguyen <anhnguyenis@gmail.com>
2019-11-29 09:54:51 +0000

    add title to bookmark_manager and bookmark_manager_test databases

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -24,7 +24,7 @@ class Bookmark
     else
     connection = PG.connect(dbname: 'bookmark_manager')
     end
-    connection.exec("INSERT INTO bookmarks(url) VALUES('#{url}')")
+    connection.exec("INSERT INTO bookmarks(url) VALUES('#{url}');")
 
   end
 end
--- spec/features/web_helper.rb
+++ spec/features/web_helper.rb
@@ -2,14 +2,13 @@ require './app'
 require 'pg'
 
 def setup_table
-# connection = PG.connect(dbname: 'bookmark_manager_test')
-# connection.exec('DROP TABLE IF EXISTS bookmarks;')
-# connection.exec('CREATE TABLE bookmarks(id SERIAL PRIMARY KEY,url VARCHAR(60));')
+connection = PG.connect(dbname: 'bookmark_manager_test')
+connection.exec('DROP TABLE IF EXISTS bookmarks;')
+connection.exec('CREATE TABLE bookmarks(id SERIAL PRIMARY KEY,url VARCHAR(60));')
 # connection.exec("INSERT INTO bookmarks(url) VALUES('http://www.makersacademy.com')")
 # connection.exec("INSERT INTO bookmarks(url) VALUES('www.bbc.co.uk')")
 # connection.exec("INSERT INTO bookmarks(url) VALUES('http://google.com')")
-
-Bookmark.add(url: "http://www.makersacademy.com") #replaces web_helper named setup_table
-Bookmark.add(url: "http://www.bbc.co.uk")
-Bookmark.add(url: "http://www.google.com")
+Bookmark.add("http://www.makersacademy.com") #replaces web_helper named setup_table
+Bookmark.add("www.bbc.co.uk")
+Bookmark.add("http://google.com")
 end
